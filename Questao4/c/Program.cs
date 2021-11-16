6c088bad0652394301370979e29ce33674cf3d40
Jonathan Barcela <jonathan.barcela@cwi.com.br>
2021-11-16 16:46:04 -0300

    Respostas 4 e 5

---
+++ Questao4/c/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Questao4
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            ClasseTeste classeTeste = new ClasseTeste(String.Empty);
+        }
+    }
+
+    public class ClasseTeste
+    {
+        public ClasseTeste(string propriedadeObrigatoria)
+        {
+            if (string.IsNullOrEmpty(propriedadeObrigatoria))
+            {
+                throw new ArgumentNullException(nameof(propriedadeObrigatoria), "Esta propriedade é obrigatória");
+            }
+
+            PropriedadeObrigatoria = propriedadeObrigatoria;
+        }
+
+        public string PropriedadeObrigatoria { get; private set; }
+    }
+}
